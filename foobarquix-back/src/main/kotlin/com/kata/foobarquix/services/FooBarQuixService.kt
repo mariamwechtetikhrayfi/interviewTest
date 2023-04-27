8cbe8537629b78ac6043272c4dc1aa39912df325
mariamwechtetikhrayfi <mariamwechtetikhrayfi@gmail.com>
2023-04-27 07:48:02 +0200

    refactoring backend code

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -7,23 +7,16 @@ class FooBarQuixService {
 
     fun convertNumber(inputNumber: Int): String {
         var result = ""
-        if (inputNumber % 3 === 0) {
-            result += "FOO"
+        if (inputNumber % 3 == 0) result += "FOO"
+        if (inputNumber % 5 == 0) result += "BAR"
+        if (inputNumber % 7 == 0) result += "QIX"
+        inputNumber.toString().forEach {
+            when (it) {
+                '3' -> result += "FOO"
+                '5' -> result += "BAR"
+                '7' -> result += "QIX"
+            }
         }
-        if (inputNumber % 5 === 0) {
-            result += "BAR"
-        }
-        if (inputNumber % 7 === 0) {
-            result += "QIX"
-        }
-        val toFooBar: String = inputNumber.toString()
-        for (j in 0 until toFooBar.length) {
-            val element: Char = toFooBar.get(j)
-            if (element == '3') result += "FOO"
-            if (element == '5') result += "BAR"
-            if (element == '7') result += "QIX"
-        }
-        return if (result.isEmpty()) toFooBar else result
+        return if (result.isEmpty()) inputNumber.toString() else result
     }
-
 }
