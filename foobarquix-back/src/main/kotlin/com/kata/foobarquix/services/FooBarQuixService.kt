b7a2d559a792abcfa5b8161fe8ba5b2f89a615a1
mariamwechtetikhrayfi <mariamwechtetikhrayfi@gmail.com>
2023-05-03 17:14:10 +0200

    refactor code

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -5,18 +5,49 @@ import org.springframework.stereotype.Component
 @Component
 class FooBarQuixService {
 
+    /*    fun convertNumber(inputNumber: Int): String {
+            var result = ""
+            if (inputNumber % 3 == 0) result += "FOO"
+            if (inputNumber % 5 == 0) result += "BAR"
+            if (inputNumber % 7 == 0) result += "QIX"
+            inputNumber.toString().forEach {
+                when (it) {
+                    '3' -> result += "FOO"
+                    '5' -> result += "BAR"
+                    '7' -> result += "QIX"
+                }
+            }
+            return if (result.isEmpty()) inputNumber.toString() else result
+        }
+
+
+     */
     fun convertNumber(inputNumber: Int): String {
         var result = ""
-        if (inputNumber % 3 == 0) result += "FOO"
-        if (inputNumber % 5 == 0) result += "BAR"
-        if (inputNumber % 7 == 0) result += "QIX"
-        inputNumber.toString().forEach {
-            when (it) {
-                '3' -> result += "FOO"
-                '5' -> result += "BAR"
-                '7' -> result += "QIX"
+        val fooBarQuixMap = mapOf(3 to "FOO", 5 to "BAR", 7 to "QIX")
+         result += applyDivisiblityRules(inputNumber, fooBarQuixMap)
+         result += applyDigitRules(inputNumber, fooBarQuixMap)
+        return if (result.isEmpty()) inputNumber.toString()
+        else result
+    }
+
+    fun applyDivisiblityRules(inputNumber: Int, fooBarQixMap: Map<Int, String>): String {
+        var result = ""
+        fooBarQixMap.keys.forEach {
+            if (inputNumber % it == 0) {
+                result += fooBarQixMap[it]
             }
         }
-        return if (result.isEmpty()) inputNumber.toString() else result
+        return result
+    }
+
+    fun applyDigitRules(inputNumber: Int, fooBarQixMap: Map<Int, String>): String {
+        var result = ""
+        inputNumber.toString().forEach {
+            fooBarQixMap[it.toString().toInt()]?.let {
+                result += it
+            }
+        }
+        return result
     }
 }
